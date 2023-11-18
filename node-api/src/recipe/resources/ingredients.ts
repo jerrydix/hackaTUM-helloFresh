2df13276d45d62e6143954215dcfeb7444a42ce3
Roman Shchekotov (Doomie) <rom.shchekotov@gmail.com>
2023-11-18 20:16:52 +0100

    added recipes

--- node-api/src/recipe/resources/ingredients.ts
+++ node-api/src/recipe/resources/ingredients.ts
@@ -1,33 +1,40 @@
 // noinspection SpellCheckingInspection
 export default {
-  "DAIRY_AND_EGGS": ["butter", "egg", "greek yogurt", "heavy cream", "parmesan cheese", "cheddar cheese"],
+  "DAIRY_AND_EGGS": [
+    "butter", "egg", "greek yogurt", "heavy cream",
+    "parmesan cheese", "mozzarella", "cheddar cheese"
+  ],
   "MEAT": ["chicken breast", "bacon"],
   "SEAFOOD": [],
   "VEGETABLES": [
     "tomato", "bell pepper", "zucchini", "eggplant", "sweet potato",
     "onion", "lettuce", "red bell pepper", "cucumber", "red onion", "ginger",
     "portobello mushroom", "broccoli", "carrot", "snap peas", "chives",
-    "potato", "cauliflower",
+    "potato", "cauliflower", "baby spinach"
   ],
   "FRUITS": [],
   "GRAINS_AND_CEREALS": [
     "whole wheat wrap", "rigatoni pasta", "basmati rice",
-    "breadcrumbs", "conchiglie pasta"
+    "breadcrumbs", "conchiglie pasta", "spaghetti"
   ],
   "LEGUMES": ["chickpeas", "peanuts"],
-  "NUTS_AND_SEEDS": [],
+  "NUTS_AND_SEEDS": [
+    "pumpkin seed", "pine nut"
+  ],
   "HERBS_AND_SPICES": [
     "basil", "oregano", "chili flakes", "garlic", "cumin",
     "coriander", "parsley", "turmeric", "garam masala",
     "paprika", "cilantro", "garlic powder", "rosemary", "thyme"
   ],
   "SWEETENERS": ["honey"],
-  "OILS_AND_FATS": ["olive oil", "vegetable oil", "sesame oil"],
+  "OILS_AND_FATS": [
+    "olive oil", "vegetable oil", "sesame oil", "pumpkin seed oil"
+  ],
   "BEVERAGES": [],
   "BAKING_AND_COOKING_INGREDIENTS": ["flour", "soy sauce"],
   "CONDIMENTS_AND_SAUCES": [
     "sweet chili sauce", "tahini", "lemon juice", "salt", "pepper",
-    "ketchup", "peanut butter"
+    "ketchup", "peanut butter", "vinegar"
   ],
   "PROCESSED_FOODS": ["kalamata olives"],
   "MISC": [],
--- node-api/src/recipe/resources/utensils.ts
+++ node-api/src/recipe/resources/utensils.ts
@@ -2,5 +2,5 @@ export default [
   "colander", "large pot", "mixing bowl", "frying pan", "knife",
   "cutting board", "food processor", "cheese grater", "measuring spoons",
   "spatula", "shallow dishes", "whisk", "tongs", "wok", "oven",
-  "baking sheet", "potato masher", "pot",
+  "baking sheet", "potato masher", "pot", "salad bowl", "blender"
 ];
