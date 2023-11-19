96e7aab403bdc8ddf2c717d02b461ab2d4b1193f
J3rry <jerrygamerhd@gmail.com>
2023-11-19 05:39:01 +0100

    stuff2

--- flutter_version/lib/pages/landing_page.dart
+++ flutter_version/lib/pages/landing_page.dart
@@ -1,8 +1,11 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_version/RecipeManager.dart';
+import 'package:flutter_version/widgets/toggle_ingredientType.dart';
 
 import '../rest/Recipe.dart';
 import '../widgets/recipe_card.dart';
+import '../widgets/toggle_product.dart';
 class LandingPage extends StatefulWidget {
   const LandingPage({super.key});
   @override
@@ -11,11 +14,18 @@ class LandingPage extends StatefulWidget {
 
 class _LandingPageState extends State<LandingPage> {
 
-    @override
+  List<String> ingredients = RecipeManager.instance.ingredientTypesFormatted;
+  List<String> selectedIngredients = [];
+  List<String> notSelectedIngredients = [];
+
+  @override
     Widget build(BuildContext context) {
       return Scaffold(
-          body: SingleChildScrollView(
+          body: Center(
+          child: SingleChildScrollView(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 const Image (
                   image: AssetImage('assets/OrangeFinal.png'),
@@ -23,28 +33,62 @@ class _LandingPageState extends State<LandingPage> {
                   width: 200,
                 ),
                 Padding(
-                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: MediaQuery.of(context).size.width * 1 / 10),
-                  child: Text("Let's start by selecting your favorite ingredients:",
+                  padding:  EdgeInsets.symmetric(vertical: 15, horizontal: MediaQuery.of(context).size.width * 1 / 10),
+                  child: Text("Let's start by selecting your favorite ingredient categories:",
                     style: Theme.of(context).textTheme.headlineMedium,
                     textAlign: TextAlign.center,
                   ),
                 ),
-                SizedBox(
-                  height: 10, //TODO
+                Container(
+                  width: MediaQuery.of(context).size.width * 5 / 10,
+                  alignment: Alignment.center,
+                  child: Wrap(
+                    children: List<Widget>.generate(
+                      ingredients.length,
+                          (int index) => toggleIngredient(title: ingredients[index], value: 1,),
+                    ),
+                  ),
                 ),
                 Padding(
-                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: MediaQuery.of(context).size.width * 1 / 10),
+                  padding: EdgeInsets.symmetric(vertical: 15, horizontal: MediaQuery.of(context).size.width * 1 / 10),
                   child: Text("Select any dietary restrictions you may have:",
                     style: Theme.of(context).textTheme.headlineMedium,
                     textAlign: TextAlign.center,
                   ),
                 ),
-                Column(
-                  children: createAllergyTiles(),
+                Container(
+                  width: MediaQuery.of(context).size.width * 5 / 10,
+                  alignment: Alignment.center,
+                  child: Wrap(
+                    children: createAllergyTiles(),
+                  ),
                 ),
-
+                Padding(
+                  padding:  EdgeInsets.symmetric(vertical: 15, horizontal: MediaQuery.of(context).size.width * 1 / 10),
+                  child: Text("Alright, now let's get cooking!",
+                    style: Theme.of(context).textTheme.headlineMedium,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    minimumSize: MaterialStateProperty.all(const Size(200, 100)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                      ),
+                    ),
+                    textStyle: MaterialStateProperty.all(Theme.of(context).textTheme.headlineMedium
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, "/home");
+                  },
+                  child: const Text("Save Preferences"),
+                ),
               ],
             ),
+          ),
           ),
       );
     }
@@ -53,20 +97,27 @@ class _LandingPageState extends State<LandingPage> {
       List<Widget> tiles = [];
       for (var allergy in RecipeManager.instance.allAllergyStrings) {
         tiles.add(
-          CheckboxListTile(
-            title: Text(allergy),
-            value: false,
-            onChanged: (newValue) {
-              setState(() {
-                //TODO
-              });
-            },
-            controlAffinity: ListTileControlAffinity.leading,  //  <-- leading Checkbox
-          ),
+          Container(
+            alignment: Alignment.center,
+            width: 225,
+            child: CheckboxListTile(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              checkboxShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+              title: Text(allergy),
+              value: false,
+              onChanged: (newValue) {
+                setState(() {
+                  //TODO
+                });
+              },
+              controlAffinity: ListTileControlAffinity.leading,
+            ),
+          )
         );
       }
       print(tiles.length);
       return tiles;
     }
-
 }
---
+++ flutter_version/lib/widgets/toggle_ingredientType.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_version/RecipeManager.dart';
+
+class toggleIngredient extends StatefulWidget {
+  const toggleIngredient({super.key, required this.title, required this.value});
+
+  final String title;
+  final int value;
+
+
+  @override
+  _toggleProductState createState() => _toggleProductState();
+}
+
+class _toggleProductState extends State<toggleIngredient> {
+  late int choice;
+  Color currentColor = Colors.red;
+
+  @override
+  void initState() {
+    choice = widget.value;
+    switch (choice) {
+      case 0: currentColor = Colors.redAccent!;
+      break;
+      case 1: currentColor = Colors.white!;
+      break;
+      case 3: currentColor =  Colors.green[400]!;
+    }
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(right: 5, left: 5, top: 5, bottom: 5),
+      child: Card(
+        elevation: 3,
+        color: currentColor,
+        child: Padding(
+            padding: EdgeInsets.only(right: 5, left: 5, top: 5),
+            child: SizedBox(
+              width: 160,
+              height: 80,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(widget.title, style: Theme
+                        .of(context)
+                        .textTheme
+                        .bodyMedium, textAlign: TextAlign.center,),
+                    Padding(padding: EdgeInsets.only(bottom: 5),
+                      child: SegmentedButton<int>(
+                        style: ButtonStyle(shape: MaterialStateProperty.all(
+                            RoundedRectangleBorder(borderRadius: BorderRadius
+                                .circular(8))),
+                          surfaceTintColor: MaterialStateProperty.all(Colors.white)
+                        ),
+                        showSelectedIcon: false,
+                        segments: const <ButtonSegment<int>>[
+                          ButtonSegment<int>(
+                              value: 0,
+                              //label: Text('Day'),
+                              icon: Icon(Icons.close)),
+                          ButtonSegment<int>(
+                              value: 1,
+                              //label: Text('Week'),
+                              icon: Icon(Icons.remove)),
+                          ButtonSegment<int>(
+                              value: 2,
+                              //label: Text('Month'),
+                              icon: Icon(Icons.check)),
+                        ],
+                        selected: <int>{choice},
+                        onSelectionChanged: (Set<int> value) {
+                          Color? color = Theme
+                              .of(context)
+                              .colorScheme
+                              .primaryContainer;
+                          switch (value.first) {
+                          case 0: color = Colors.redAccent;
+                          for (var e in RecipeManager.instance.ingredientTypesSelected) {
+                            if (e == widget.title) {
+                              RecipeManager.instance.ingredientTypesSelected.remove(e);
+                              break;
+                            }
+                          }
+                          RecipeManager.instance.ingredientTypesNotSelected.add(widget.title);
+                          break;
+                          case 1: color = Colors.white!;
+                          bool inFirst = false;
+                          for (var e in RecipeManager.instance.ingredientTypesNotSelected) {
+                            if (e == widget.title) {
+                              inFirst = true;
+                              RecipeManager.instance.ingredientTypesNotSelected.remove(e);
+                              break;
+                            }
+                          }
+                          if (!inFirst) {
+                            for (var e in RecipeManager.instance.ingredientTypesSelected) {
+                              if (e == widget.title) {
+                                RecipeManager.instance.ingredientTypesSelected.remove(e);
+                                break;
+                              }
+                            }
+                          }
+                          break;
+                          case 2: color = Colors.green[400]!;
+                          for (var e in RecipeManager.instance.ingredientTypesNotSelected) {
+                            if (e == widget.title) {
+                              RecipeManager.instance.ingredientTypesNotSelected.remove(e);
+                              break;
+                            }
+                          }
+                          RecipeManager.instance.ingredientTypesSelected.add(widget.title);
+                          break;
+                          }
+                          setState(() {
+                            choice = value.first;
+                            currentColor = color!;
+                          });
+                          print(RecipeManager.instance.ingredientTypesNotSelected);
+                          print(RecipeManager.instance.ingredientTypesSelected);
+                        },
+                      ),
+                    )
+                  ]
+              ),
+
+            )
+        ),
+      ),);
+  }
+}
